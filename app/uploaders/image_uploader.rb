88044a5e41d7f40726489f06b96f278f6679acd9
Luke <luke.rogers1843@gmail.com>
2019-06-09 17:59:01 +0000

    whitelist C9 preview IP

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,12 +5,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   
-  if Rails.env.production?
-  storage :fog
-  else
-  storage :file
-  end
-
+    storage :fog
+  
   # Directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   # def store_dir
